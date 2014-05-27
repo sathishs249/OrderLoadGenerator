b54be16d7bb5b65d5187cbcc1d1fb8b80f1a9164
divyavani <divya.pemmasani11@gmailcom>
2014-05-27 11:44:55 +0530

    Update PersonInfoGenarator.java

--- PerformanceDataLoadGenerator/src/com/gsot/personinfo/PersonInfoGenarator.java
+++ PerformanceDataLoadGenerator/src/com/gsot/personinfo/PersonInfoGenarator.java
@@ -1,14 +1,16 @@
 package com.gsot.personinfo;
 
+package com.gsot.personinfo;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
 import com.yantra.yfc.dom.YFCDocument;
 import com.yantra.yfc.dom.YFCElement;
-//import  scala.collection.JavaConversions; 
+import  scala.collection.JavaConversions; 
 //import static com.gsot.personinfo.PersonInfoGen;
-public class PersonInfoGenarator extends CombinationGenarator{
+public class PersonInfoGenarator {
 
 	/**
 	 * @param args
@@ -17,11 +19,20 @@ public class PersonInfoGenarator extends CombinationGenarator{
 	 */
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		//get input XML pass the values to there methods
-		YFCDocument orderLoadGenerator = YFCDocument.getDocumentForXMLFile("F:\\FromPC\\Sathish\\Performance\\PerformanceDataLoadGenerator\\input\\orderLoadGenerator.xml");
-		process(orderLoadGenerator);
+		YFCDocument orderLoadGenerator = YFCDocument.getDocumentForXMLFile("C:\\Users\\GSOT\\Desktop\\exlipse\\test1\\Perfomance\\inputs\\orderLoadGenerator.xml");
+		YFCElement orderLoadGeneratorEle = orderLoadGenerator.getDocumentElement();
+		YFCElement orderPacing = orderLoadGeneratorEle.getChildElement("OrderPacing");
+		String numberOfOrders = orderPacing.getAttribute("NumberOfOrders");
+		
+	
+		for(int i=0;i<Integer.parseInt(numberOfOrders);i++){
+			String personInfoBillToDoc= process(orderLoadGenerator);
+			System.out.println("peronInfo bill to :"+ i+ " values :"+personInfoBillToDoc);
+		}	
 	}
-	public static  void process(YFCDocument orderLoadGenerator) throws IOException{
-		System.out.println("orderLoadGenerator"+orderLoadGenerator);
+	
+	private static String process(YFCDocument orderLoadGenerator) throws IOException{
+		
 		YFCElement orderLoadGeneratorEle = orderLoadGenerator.getDocumentElement();
 		YFCElement personInfoGenerator = orderLoadGeneratorEle.getChildElement("PersonInfoGenerator");
 		YFCElement firstName = personInfoGenerator.getChildElement("FirstName");
@@ -30,76 +41,68 @@ public class PersonInfoGenarator extends CombinationGenarator{
 		YFCElement lastName = personInfoGenerator.getChildElement("LastName");
 		String lastNamefile = lastName.getAttribute("File");
 		String lastNamecount =  lastName.getAttribute("Count");
-		YFCElement orderPacing = orderLoadGeneratorEle.getChildElement("OrderPacing");
-		String numberOfOrders = orderPacing.getAttribute("NumberOfOrders");
-//new com.gsot.personinfo.PersonInfoGen.getValue();
-//PersonInfoGen.initFirstName();
-		//method call
-		ArrayList<ArrayList<String>> totalcombinationList = getCombinationsList(firstNamefile,firstNamecount,lastNamefile,lastNamecount);
-		Random r = new Random();
-		for(int i=0;i<Integer.parseInt(numberOfOrders);i++){
 		
-			ArrayList<String> list = totalcombinationList.get(r.nextInt(totalcombinationList.size()));
-			System.out.println("total list :"+list);
-			createPersonInfoBillTo(list);
-			createPersonInfoShipTo(list);
-
-		}
+		//method call
+		ArrayList<PersonInfoObject> totalcombinationList = CombinationGenarator.getCombinationsList(firstNamefile,firstNamecount,lastNamefile,lastNamecount);
+		Random r = new Random();
+		PersonInfoObject personInfoObject  = totalcombinationList.get(r.nextInt(totalcombinationList.size()));
+		String personInfoBillToDoc = createPersonInfoBillTo(personInfoObject);
+		String personInfoShipToDoc = createPersonInfoShipTo(personInfoObject);
+		
+		return personInfoBillToDoc+personInfoShipToDoc;
 	}
-	private static YFCDocument createPersonInfoBillTo(ArrayList<String> list){
-			
+	private static String createPersonInfoBillTo(PersonInfoObject personInfoObject){
+		
 			YFCDocument personInfoBillTo = YFCDocument.createDocument("PersonInfoBillTo");	
 			YFCElement personInfoBillToEle = personInfoBillTo.getDocumentElement();
-			personInfoBillToEle.setAttribute("AddressLine1", list.get(7));
+			personInfoBillToEle.setAttribute("AddressLine1", personInfoObject.getAddress());
 			personInfoBillToEle.setAttribute("AddressLine2", "");
 			personInfoBillToEle.setAttribute("AddressLine3", "");
 			personInfoBillToEle.setAttribute("AddressLine4", "");
 			personInfoBillToEle.setAttribute("AddressLine5", "");
-			personInfoBillToEle.setAttribute("City", list.get(3));
+			personInfoBillToEle.setAttribute("City", personInfoObject.getCity());
 			personInfoBillToEle.setAttribute("Country", "");
-			personInfoBillToEle.setAttribute("DayPhone", list.get(6));
-			personInfoBillToEle.setAttribute("EMailID", list.get(2));
-			personInfoBillToEle.setAttribute("FirstName", list.get(0));
+			personInfoBillToEle.setAttribute("DayPhone", personInfoObject.getPhoneNo());
+			personInfoBillToEle.setAttribute("EMailID", personInfoObject.getEmailID());
+			personInfoBillToEle.setAttribute("FirstName", personInfoObject.getFirstName());
 			personInfoBillToEle.setAttribute("JobTitle", "");
-			personInfoBillToEle.setAttribute("LastName", list.get(1));
+			personInfoBillToEle.setAttribute("LastName", personInfoObject.getLastName());
 			personInfoBillToEle.setAttribute("MiddleName", "");
-			personInfoBillToEle.setAttribute("MobilePhone", list.get(6));
+			personInfoBillToEle.setAttribute("MobilePhone", personInfoObject.getPhoneNo());
 			personInfoBillToEle.setAttribute("PersonID", "");
-			personInfoBillToEle.setAttribute("State", list.get(4));
+			personInfoBillToEle.setAttribute("State", personInfoObject.getState());
 			personInfoBillToEle.setAttribute("Suffix", "");
 			personInfoBillToEle.setAttribute("Title", "");
-			personInfoBillToEle.setAttribute("ZipCode", list.get(5));
-			
-			System.out.println(personInfoBillTo);		
-			return personInfoBillTo;
+			personInfoBillToEle.setAttribute("ZipCode", personInfoObject.getZipcode());
+				
+			return personInfoBillTo.toString();
 	}
-	private static YFCDocument createPersonInfoShipTo(ArrayList<String> list){
+	private static String createPersonInfoShipTo(PersonInfoObject personInfoObject){
 		
 		YFCDocument personInfoBillTo = YFCDocument.createDocument("PersonInfoShipTo");	
 		YFCElement personInfoBillToEle = personInfoBillTo.getDocumentElement();
-		personInfoBillToEle.setAttribute("AddressLine1", list.get(7));
+		personInfoBillToEle.setAttribute("AddressLine1", personInfoObject.getAddress());
 		personInfoBillToEle.setAttribute("AddressLine2", "");
 		personInfoBillToEle.setAttribute("AddressLine3", "");
 		personInfoBillToEle.setAttribute("AddressLine4", "");
 		personInfoBillToEle.setAttribute("AddressLine5", "");
-		personInfoBillToEle.setAttribute("City", list.get(3));
+		personInfoBillToEle.setAttribute("City", personInfoObject.getCity());
 		personInfoBillToEle.setAttribute("Country", "");
-		personInfoBillToEle.setAttribute("DayPhone", list.get(6));
-		personInfoBillToEle.setAttribute("EMailID", list.get(2));
-		personInfoBillToEle.setAttribute("FirstName", list.get(0));
+		personInfoBillToEle.setAttribute("DayPhone", personInfoObject.getPhoneNo());
+		personInfoBillToEle.setAttribute("EMailID", personInfoObject.getEmailID());
+		personInfoBillToEle.setAttribute("FirstName", personInfoObject.getFirstName());
 		personInfoBillToEle.setAttribute("JobTitle", "");
-		personInfoBillToEle.setAttribute("LastName", list.get(1));
+		personInfoBillToEle.setAttribute("LastName", personInfoObject.getLastName());
 		personInfoBillToEle.setAttribute("MiddleName", "");
-		personInfoBillToEle.setAttribute("MobilePhone", list.get(6));
+		personInfoBillToEle.setAttribute("MobilePhone", personInfoObject.getPhoneNo());
 		personInfoBillToEle.setAttribute("PersonID", "");
-		personInfoBillToEle.setAttribute("State", list.get(4));
+		personInfoBillToEle.setAttribute("State", personInfoObject.getState());
 		personInfoBillToEle.setAttribute("Suffix", "");
 		personInfoBillToEle.setAttribute("Title", "");
-		personInfoBillToEle.setAttribute("ZipCode", list.get(5));
+		personInfoBillToEle.setAttribute("ZipCode", personInfoObject.getZipcode());
 		
-		System.out.println(personInfoBillTo);		
-		return personInfoBillTo;
-}
-
-
+		
+		//System.out.println(personInfoBillTo);		
+		return personInfoBillTo.toString();
+	}
 }
