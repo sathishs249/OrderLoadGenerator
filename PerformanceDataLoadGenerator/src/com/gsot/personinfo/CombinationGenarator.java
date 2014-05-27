6fb0964d6bed8a618a391a5620d6b24bfd160043
divyavani <divya.pemmasani11@gmailcom>
2014-05-27 11:46:42 +0530

    Update CombinationGenarator.java

--- PerformanceDataLoadGenerator/src/com/gsot/personinfo/CombinationGenarator.java
+++ PerformanceDataLoadGenerator/src/com/gsot/personinfo/CombinationGenarator.java
@@ -1,45 +1,45 @@
 package com.gsot.personinfo;
 
+i
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
-import com.gsot.utils.FileProcessing;
 
-
-public class CombinationGenarator extends FileProcessing {
+public class CombinationGenarator {
 	
-	public static ArrayList<ArrayList<String>> getCombinationsList(String firstNamefile, String firstNamecount, String lastNamefile, String lastNamecount) throws IOException{
-		 ArrayList<ArrayList<String>> totalList = new ArrayList<ArrayList<String>>();
+	public static ArrayList<PersonInfoObject> getCombinationsList(String firstNamefile, String firstNamecount, String lastNamefile, String lastNamecount) throws IOException{
+		 ArrayList<PersonInfoObject> totalList = new ArrayList<PersonInfoObject>();
 		 ArrayList<String> listFirstName = FileProcessing.getFistNameList(firstNamefile, firstNamecount);
 		 ArrayList<String> listLastName = FileProcessing.getLastNameList(lastNamefile, lastNamecount);
+		 
 		 for(int i=0;i<listFirstName.size();i++){
 				for(int j=0;j<listLastName.size();j++){
-	        	   ArrayList<String> list = new ArrayList<String>();
-	        	   System.out.println("Name :"+listFirstName.get(i)+" "+listLastName.get(j));   
+	        	   PersonInfoObject personInfoObject = new PersonInfoObject();
+	        	   //System.out.println("Name :"+listFirstName.get(i)+" "+listLastName.get(j));   
 	        	   String emailID = getEmailID(listFirstName.get(i).toString(),listLastName.get(j).toString());
 	        	   int value = getValue(listFirstName.get(i)+listLastName.get(j));
-	        	   ArrayList<String> cityziplist = getCityZip(value);
-	        	   String city = getCity(cityziplist);
-	        	   String state = getState(cityziplist);
-	        	   String zipcode = getZipcode(cityziplist);
+	        	   AddressObject cityzipobj = getCityZip(value);
+	        	   String city = getCity(cityzipobj);
+	        	   String state = getState(cityzipobj);
+	        	   String zipcode = getZipcode(cityzipobj);
 	        	   String phone = getPhone(value);
 	        	   String address = getAddress(value,listLastName.get(j));
 	        	   
-	        	   list.add(listFirstName.get(i));
-	        	   list.add(listLastName.get(j));
-	        	   list.add(emailID);
-	        	   list.add(city);
-	        	   list.add(state);
-	        	   list.add(zipcode);
-	        	   list.add(phone);
-	        	   list.add(address);
-	        	   System.out.println("list :"+list);
-	        	   totalList.add(list);
+	        	   personInfoObject.setFirstName(listFirstName.get(i));
+	        	   personInfoObject.setLastName(listLastName.get(j));
+	        	   personInfoObject.setEmailID(emailID);
+	        	   personInfoObject.setCity(city);
+	        	   personInfoObject.setState(state);
+	        	   personInfoObject.setZipcode(zipcode);
+	        	   personInfoObject.setPhoneNo(phone);
+	        	   personInfoObject.setAddress(address);
+	        	   totalList.add(personInfoObject);
+	        	  
 	           }
 		 }
-		System.out.println(totalList);
+		//System.out.println(totalList.toString());
 		return totalList;
 		
 	}
@@ -57,26 +57,28 @@ public class CombinationGenarator extends FileProcessing {
 		return a;
 	}
 	
-	private static ArrayList<String> getCityZip(int value) throws IOException{
-		LinkedHashMap<String, ArrayList<String>> Listcityzip = listcityzip();
+	private static AddressObject getCityZip(int value) throws IOException{
+		String file = "C:\\Users\\GSOT\\Desktop\\exlipse\\test1\\Perfomance\\inputs\\cityzip.txt";
+	
+		LinkedHashMap<String, ArrayList<String>> Listcityzip = FileProcessing.listcityzip(file);
 		int cityzipsize = Listcityzip.size();
  	    int citvalue = value%cityzipsize;
- 	   LinkedHashMap<String, ArrayList<String>> cityzipList =getcityzip(citvalue);
-		 ArrayList<String> list = new ArrayList<String>();
-		 for (Entry<String, ArrayList<String>> entry : cityzipList.entrySet()) { 
-			 list = entry.getValue();
+ 	   LinkedHashMap<String, AddressObject> cityzipList =FileProcessing.getcityzip(citvalue,file);
+		 AddressObject addressObject = new AddressObject();
+		 for (Entry<String, AddressObject> entry : cityzipList.entrySet()) { 
+			 addressObject = entry.getValue();
       }
-		return list;
+		return addressObject;
 	}
 	
-	private static String getCity(ArrayList<String> cityziplist) throws IOException{
-		 return cityziplist.get(0);
+	private static String getCity(AddressObject cityzipobj) throws IOException{
+		 return cityzipobj.getCity();
 	}
-	private static String getState(ArrayList<String> cityziplist) throws IOException{
-		 return cityziplist.get(1);
+	private static String getState(AddressObject cityzipobj) throws IOException{
+		 return cityzipobj.getState();
 	}
-	private static String getZipcode(ArrayList<String> cityziplist) throws IOException{
-		 return cityziplist.get(2);
+	private static String getZipcode(AddressObject cityzipobj) throws IOException{
+		 return cityzipobj.getZipcode();
 	}
 	private static String getPhone(int value) throws IOException{
 		Long val = Long.parseLong(String.valueOf(value));
