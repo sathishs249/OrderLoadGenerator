6725f89a077f612eb501332a51584be46fcec3ba
divyavani <divya.pemmasani11@gmailcom>
2014-05-27 11:50:05 +0530

    Create AddressObject.java
    create new addresObject class

---
+++ PerformanceDataLoadGenerator/src/com/gsot/personinfo/AddressObject.java
@@ -0,0 +1,25 @@
+package com.gsot.personinfo;
+
+public class AddressObject {
+	private String city;
+	private String state;
+	private String zipcode;
+	public String getCity() {
+		return city;
+	}
+	public String getState() {
+		return state;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+}
