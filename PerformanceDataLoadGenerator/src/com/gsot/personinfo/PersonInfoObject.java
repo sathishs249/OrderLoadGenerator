b176371ef7b606b0b90e58d97c744b4749d57f42
divyavani <divya.pemmasani11@gmailcom>
2014-05-27 11:48:54 +0530

    Create PersonInfoObject.java

---
+++ PerformanceDataLoadGenerator/src/com/gsot/personinfo/PersonInfoObject.java
@@ -0,0 +1,63 @@
+package com.gsot.personinfo;
+
+public class PersonInfoObject {
+	private String firstName;
+	private String lastName;
+	private String  emailID;
+	private String city;
+	private String state;
+	private String phoneNo;
+	private String address;
+	private String zipcode;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public String getCity() {
+		return city;
+	}
+	public String getState() {
+		return state;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	
+}
